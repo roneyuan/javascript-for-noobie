ac82a7c060894b8fa25d9bf9931b067311a49d1f
roneyuan <roneyuan_m03@hotmail.com>
2017-03-25 23:40:05 -0400

    Update declartion vs expression

--- es6/declaration-vs-expression.js
+++ es6/declaration-vs-expression.js
@@ -1,3 +1,15 @@
+/* No Hositing */
+openDoor();
+
+/* Hoisting */
+closeDoor();
+
 // Function Expression
+var closeDoor = function() {
+	console.log("Close the door");
+}
 
 // Function Decalration
+function openDoor() {
+	console.log("Open the door");
+}
