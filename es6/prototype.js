7ad8dad349db5031bf3107d699b2f9ab4dac7fd6
roneyuan <roneyuan_m03@hotmail.com>
2017-03-29 22:18:39 -0400

    Modify content

--- es6/prototype.js
+++ es6/prototype.js
@@ -1,9 +1,11 @@
-/* In class and function. Can be hacked, so use object 
-literal is another method
+/* 
+	Prototype is in class or function. Prototype shares the same memory. 
+	Can be hacked, so use object literal is another method.
+	Below shows you where is the prototype.
 */
 
-var obj = function() {
-	var name = "table"
+function obj() {
+	let name = "table"
 }
 
 console.log(obj.prototype);
@@ -13,4 +15,4 @@ obj.prototype.price = "10";
 var anotherObj = new Object();
 anotherObj.prototype = Object.create(obj.prototype);
 
-console.log(anotherObj);
+console.log(anotherObj.prototype.price);
