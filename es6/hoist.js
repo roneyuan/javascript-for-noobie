75bf0ca626712430d0e101686d20b54b97b3102f
roneyuan <roneyuan_m03@hotmail.com>
2017-03-29 23:12:37 -0400

    Add comment

--- es6/hoist.js
+++ es6/hoist.js
@@ -1,3 +1,12 @@
+/*
+	What is hoisting?
+	Every time that JavaScript load the files, it would read two times.
+	First time, it would push up all the variables and functions and 
+	store into the memory. It would assign an emtpy (undefined) value to 
+	the variable. When it read the second time, it will assign the 
+	actual value.
+*/
+
 function callMeMaybe() {
 	console.log(me); // Show undefined instead of showing not defined
 	var me = "maybe";
