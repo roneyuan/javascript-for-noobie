217111bf8bc6b7fdc28b15f81c4360ec86a92a1e
roneyuan <roneyuan_m03@hotmail.com>
2017-04-02 00:03:03 -0400

    Update content

--- es6/prototype.js
+++ es6/prototype.js
@@ -16,3 +16,19 @@ var anotherObj = new Object();
 anotherObj.prototype = Object.create(obj.prototype);
 
 console.log(anotherObj.prototype.price);
+
+
+function Obj(name) { // Use upper case O
+	this.name = name;
+}
+
+var obj = new Obj("cat");
+
+console.log(Obj.prototype);
+
+Obj.prototype.name = "10";
+
+var anotherObj = new Object();
+anotherObj.prototype = Object.create(Obj.prototype);
+
+console.log(anotherObj.prototype.name);
